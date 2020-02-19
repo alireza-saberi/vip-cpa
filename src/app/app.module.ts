fed2d6d9f59883d79313d7f942b6cce06ee6a75a
Alan Saberi <saberi.ar.eng@gmail.com>
2020-02-19 13:10:10 -0500

    improving routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Routes, RouterModule } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
 import { AgmCoreModule } from '@agm/core';
 import { AngularFireModule } from '@angular/fire';
+import { RouterModule } from '@angular/router';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
 
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
@@ -19,88 +18,11 @@ import {MatMenuModule} from '@angular/material/menu';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-
-import { HomeComponent } from './modules/home/home.component';
-import { OurServicesComponent } from './modules/our-services/our-services.component';
-import { ContactUsComponent } from './modules/contact-us/contact-us.component';
 import { AppState } from './core/states/app.state';
-import { AboutUsComponent } from './modules/about-us/about-us.component';
-import { PrivacyPolicyComponent } from './modules/privacy-policy/privacy-policy.component';
-import { SiteMapComponent } from './modules/site-map/site-map.component';
-import { TermsComponent } from './modules/terms/terms.component';
-import {ServiceOneComponent} from './modules/our-services/service-one/service-one.component';
-import {ServiceTwoComponent} from './modules/our-services/service-two/service-two.component';
-import {ServiceThreeComponent} from './modules/our-services/service-three/service-three.component';
-import {ServiceFourComponent} from './modules/our-services/service-four/service-four.component';
-import {ServiceFiveComponent} from './modules/our-services/service-five/service-five.component';
 import {APIKeys} from './configs/keys';
 import {AngularFireAnalyticsModule} from '@angular/fire/analytics';
-import {RebrandingComponent} from './modules/rebranding/rebranding.component';
+import {appRoutingModule} from './app.routing';
 
-const routesArrayOfObject: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'our-services',
-    component: OurServicesComponent,
-    children: [
-      {
-        path: 'personal-tax',
-        component: ServiceOneComponent
-      },
-      {
-        path: 'corporate-tax',
-        component: ServiceTwoComponent
-      },
-      {
-        path: 'bookkeeping',
-        component: ServiceThreeComponent
-      },
-      {
-        path: 'payroll',
-        component: ServiceFourComponent
-      },
-      {
-        path: 'financial-statements',
-        component: ServiceFiveComponent
-      },
-      {
-        path: '**',
-        redirectTo: 'personal-tax'
-      }
-    ]
-  },
-  {
-    path: 'about-us',
-    component: AboutUsComponent
-  },
-  {
-    path: 'contact-us',
-    component: ContactUsComponent
-  },
-  {
-    path: 'privacy-policy',
-    component: PrivacyPolicyComponent
-  },
-  {
-    path: 'terms-and-conditions',
-    component: TermsComponent
-  },
-  {
-    path: 'site-map',
-    component: SiteMapComponent
-  },
-  {
-    path: 'ats-consulting',
-    component: RebrandingComponent
-  },
-  {
-    path: '**',
-    redirectTo: 'home'
-  }
-  ];
 
 
 @NgModule({
@@ -110,7 +32,7 @@ const routesArrayOfObject: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routesArrayOfObject),
+    appRoutingModule,
     NgxsModule.forRoot([AppState]),
     AngularFireModule.initializeApp(APIKeys.firebaseConfig),
     AngularFireAnalyticsModule,
---
+++ src/app/app.routing.ts
@@ -0,0 +1,81 @@
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './modules/home/home.component';
+import {OurServicesComponent} from './modules/our-services/our-services.component';
+import {ServiceOneComponent} from './modules/our-services/service-one/service-one.component';
+import {ServiceTwoComponent} from './modules/our-services/service-two/service-two.component';
+import {ServiceThreeComponent} from './modules/our-services/service-three/service-three.component';
+import {ServiceFourComponent} from './modules/our-services/service-four/service-four.component';
+import {ServiceFiveComponent} from './modules/our-services/service-five/service-five.component';
+import {AboutUsComponent} from './modules/about-us/about-us.component';
+import {ContactUsComponent} from './modules/contact-us/contact-us.component';
+import {PrivacyPolicyComponent} from './modules/privacy-policy/privacy-policy.component';
+import {TermsComponent} from './modules/terms/terms.component';
+import {SiteMapComponent} from './modules/site-map/site-map.component';
+import {RebrandingComponent} from './modules/rebranding/rebranding.component';
+
+const routesArrayOfObject: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'our-services',
+    component: OurServicesComponent,
+    children: [
+      {
+        path: 'personal-tax',
+        component: ServiceOneComponent
+      },
+      {
+        path: 'corporate-tax',
+        component: ServiceTwoComponent
+      },
+      {
+        path: 'bookkeeping',
+        component: ServiceThreeComponent
+      },
+      {
+        path: 'payroll',
+        component: ServiceFourComponent
+      },
+      {
+        path: 'financial-statements',
+        component: ServiceFiveComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'personal-tax'
+      }
+    ]
+  },
+  {
+    path: 'about-us',
+    component: AboutUsComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
+  },
+  {
+    path: 'privacy-policy',
+    component: PrivacyPolicyComponent
+  },
+  {
+    path: 'terms-and-conditions',
+    component: TermsComponent
+  },
+  {
+    path: 'site-map',
+    component: SiteMapComponent
+  },
+  {
+    path: 'ats-consulting',
+    component: RebrandingComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
+
+export const appRoutingModule = RouterModule.forRoot(routesArrayOfObject);
